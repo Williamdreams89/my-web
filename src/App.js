45924f5e0b7b740c983b64f6242f5f6afea6e7d1
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2022-10-21 02:07:44 +0000

    Added app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import './App.css';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Home from './components/home/Home';
+import Navigation from './components/navigation/Navigation';
+import Portfolio from './components/portfolio/Portfolio';
+import Services from './components/services/Services';
 
-const App = () => {
+function App() {
   return (
-    <div>Welcome to my web</div>
-  )
+    <div className="App">
+      <Navigation />
+      <Home />
+      <About />
+      <Portfolio />
+      <Services />
+      <Contact />
+    </div>
+  );
 }
 
-export default App
+export default App;
